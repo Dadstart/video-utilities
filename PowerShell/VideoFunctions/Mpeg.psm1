fbfa35b497b8e7dd4ef3e8d5f3926ec5cc3f993d
Andrew Bishop <andrew@bishop4.com>
2025-06-20 16:42:45 -0500

    Add barebones function

---
+++ PowerShell/VideoFunctions/Mpeg.psm1
@@ -0,0 +1,10 @@
+function Get-AudioTracks {
+    param (
+        [Parameter(Mandatory)]
+        [string]$name,
+        [Parameter(Mandatory)]
+        [int]$language = 'eng'
+    )
+
+    # TODO
+}
